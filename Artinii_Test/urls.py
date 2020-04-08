30974f35041e3db4b03cf61e1c7906b27e9f6236
Adela Skotak <skotak.adela@gmail.com>
2020-04-08 15:00:31 +0200

    Added redirect for url.py

--- Artinii_Test/urls.py
+++ Artinii_Test/urls.py
@@ -2,9 +2,11 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
+from django.shortcuts import redirect
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', lambda request: redirect('api/', permanent=False)),
     path('api/', include('files.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
